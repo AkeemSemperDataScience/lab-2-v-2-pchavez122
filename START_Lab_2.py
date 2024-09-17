ce6b6a30524304c77022a31afb566470bd755787
Pat <52054814+pchavez3@users.noreply.github.com>
2024-09-16 21:21:19 -0600

    finished 3 questions

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -2,18 +2,47 @@ def lab2Question1(word):
     # Note - you'll need to change the signature (above) to match the arguments for this lab...
     # Create a function that takes in a string 
     # Return True if that string is a palindrome, False otherwise
-    pass
+    is_palindrome = False
+    reverse_word = word[::-1]
+    print(reverse_word)
+    if word == reverse_word:
+        is_palindrome = True
+    return is_palindrome
+
+# print(lab2Question1('racecar'))
 
 def lab2Question2(number_val):
     # Create a function that takes in a number
     # Return a list of the fibonacci sequence up to that number
-    pass
+    fib_seq = []
+    for number in list(range(0,number_val+1)):
+        if number == 0 or number == 1:
+            fib_seq.append(number)
+        else:
+            new_num = fib_seq[number - 1] + fib_seq[number - 2]
+            fib_seq.append(new_num)
+    return fib_seq
+
+#print(lab2Question2(20))
 
 def lab2Question3(str1, str2):
     # Create a function that takes in two strings - str1 and str2
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
-    pass
+    counter = 0
+    found = 0 
+    # find where the first one is, add counter, then delete and do it again till index/list is empty or no st2 is found
+    while found != -1:
+        found = str1.find(str2)
+        print(found)
+        if found != -1:
+            counter += 1 
+            print(found)
+            str1 = str1[found + len(str2):]
+            print(str1)
+    return counter
+
+print(lab2Question3('coding is cool, cooking with cockatool', 'oo'))
 
 def lab2Question4(list1, list2):
     # Create a function that takes in two equal length list of numbers. 
